4879ce266a81d95ceea1ff40c1a69387265b9043
Andrew Gallant <jamslam@gmail.com>
2018-11-07 07:17:55 -0500

    ci: pin to lazy_static 1.1 in CI

--- ci/script.sh
+++ ci/script.sh
@@ -2,6 +2,19 @@
 
 set -ex
 
+MSRV="1.23.0"
+
+# If we're building on 1.23, then lazy_static 1.2 will fail to build since it
+# updated its MSRV to 1.24.1. In this case, we force the use of lazy_static 1.1
+# to build on Rust 1.23.0.
+if [ "$TRAVIS_RUST_VERSION" = "$MSRV" ]; then
+    cargo update -p lazy_static --precise 1.1.0
+    # On older versions of Cargo, this apparently needs to be run twice
+    # if Cargo.lock didn't previously exist. Since this command should be
+    # idempotent, we run it again unconditionally.
+    cargo update -p lazy_static --precise 1.1.0
+fi
+
 cargo doc --verbose
 cargo build --verbose
 cargo test --verbose
