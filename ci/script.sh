b0809eee884e3e9bd4687aac97a15e93e7b5f397
Andrew Gallant <jamslam@gmail.com>
2019-06-09 09:27:36 -0400

    ci: remove minimal version check
    Because rand. Sigh.

--- ci/script.sh
+++ ci/script.sh
@@ -13,9 +13,3 @@ fi
 
 cargo doc --verbose
 cargo test --verbose
-
-if [ "$TRAVIS_RUST_VERSION" = "nightly" ]; then
-  cargo +nightly generate-lockfile -Z minimal-versions
-  cargo build --verbose
-  cargo test --verbose
-fi
